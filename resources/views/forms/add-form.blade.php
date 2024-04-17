fec9804701b8131ed4d29d5ef36c3948e21a35df
Samuel Guebo <samuelguebo@gmail.com>
2024-04-16 18:56:43 -0600

    i18n: Dropdown values and example labels

--- resources/views/forms/add-form.blade.php
+++ resources/views/forms/add-form.blade.php
@@ -4,7 +4,7 @@
         <div class="form-group col-md-6">
             <label for="language">{{__('Language')}}</label>
             <select id="language" class="custom-select mr-sm-2" name="language">
-                <option selected value="adj">{{__('Choose language')}}...</option>
+                <option selected value="">{{__('Choose language')}}...</option>
                 <option value="adj">Adioukrou</option>
                 <option value="any">Agni</option>
                 <option value="bci">Baoulé</option>
@@ -13,7 +13,7 @@
         <div class="form-group col-md-6">
             <label for="category">{{__('Grammar category')}}</label>
             <select id="category" class="custom-select mr-sm-2"  name="category">
-                <option selected value="noun">{{__('Choose category')}}...</option>
+                <option selected value="">{{__('Choose category')}}...</option>
                 <option value="noun">{{__('Noun')}}</option>
                 <option value="verb">{{__('Verbe')}}</option>
                 <option value="adv">{{__('Adverb')}}</option>
@@ -24,11 +24,11 @@
         </div>
     </div>
     <div class="lexeme-attribute definition">
-        <label>Definition</label>
+        <label>{{__('Definition')}}</label>
 
         <div class="input-group form-group">
             <div class="input-group-prepend">
-                <span class="input-group-text">Mot</span>
+                <span class="input-group-text">{{__('Word')}}</span>
 
             </div>
             <input type="text" class="form-control keyboard-input" value="{{$term}}" name="definitionLabel">
@@ -36,25 +36,25 @@
         </div>
         <div class="input-group form-group">
             <div class="input-group-prepend">
-                <span class="input-group-text">Français</span>
+                <span class="input-group-text">{{__('French')}}</span>
             </div>
             <input type="text" class="form-control" name="definitionTranslation">
         </div>
     </div>
     <div class="example">
         <div class="lexeme-attribute repeater-container">
-            <label>Example <span class="repeater-count hidden"></span></label>
+            <label>{{__('Example')}} <span class="repeater-count hidden"></span></label>
 
             <div class="input-group form-group">
                 <div class="input-group-prepend">
-                    <span class="input-group-text source-label" id="exampleLabel">Source</span>
+                    <span class="input-group-text source-label" id="exampleLabel">{{__('Example text')}}</span>
                 </div>
                 <input type="text" class="form-control keyboard-input" name="exampleLabel[]">
                 <button type="button" class="btn btn-dark keyboard-button"><i class="fa fa-keyboard-o"></i></button>
             </div>
             <div class="input-group form-group">
                 <div class="input-group-prepend">
-                    <span class="input-group-text" id="exampleTranslation">Français</span>
+                    <span class="input-group-text" id="exampleTranslation">{{__('French')}}</span>
                 </div>
                 <input type="text" class="form-control" name="exampleTranslation[]">
             </div>
@@ -62,8 +62,8 @@
         <input type="hidden" name="action" value="/wiki/create"/>
     </div>
     <div class="actions form-group">
-        <button type="button" class="btn btn-warning btn-sm btn-add"><i class="fa fa-plus"></i> Ajouter</button>
-        <button type="button" class="btn btn-danger btn-sm btn-delete hidden"><i class="fa fa-trash"></i> Supprimer</button>
+        <button type="button" class="btn btn-warning btn-sm btn-add"><i class="fa fa-plus"></i> {{__('Add')}}</button>
+        <button type="button" class="btn btn-danger btn-sm btn-delete hidden"><i class="fa fa-trash"></i> {{__('Delete')}}</button>
         <button type="submit" class="btn btn-sm btn-primary align-right"><i class="fa fa-eye"></i>  {{__('Preview')}}</button>
     </div>
 
